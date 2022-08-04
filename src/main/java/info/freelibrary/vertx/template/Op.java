8b53c8d550ce5df94d29425fa3bea802ba0fe138
Mark A. Matney, Jr <markmatney@protonmail.com>
2022-08-04 11:12:10 -0700

    Initial commit

---
+++ src/main/java/info/freelibrary/vertx/template/Op.java
@@ -0,0 +1,20 @@
+
+package info.freelibrary.vertx.template;
+
+/**
+ * Template API operation IDs.
+ */
+public final class Op {
+
+    /**
+     * A constant for the "get status" operation.
+     */
+    public static final String GET_STATUS = "getStatus";
+
+    /**
+     * Constants class' constructors should be private.
+     */
+    private Op() {
+    }
+
+}
