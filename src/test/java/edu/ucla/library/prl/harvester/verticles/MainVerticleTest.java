752bad08cd6ea1649841b224c05f614cca091e98
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2022-08-04 17:42:10 -0700

    Upgrade to JUnit 5

--- src/test/java/edu/ucla/library/prl/harvester/verticles/MainVerticleTest.java
+++ src/test/java/edu/ucla/library/prl/harvester/verticles/MainVerticleTest.java
@@ -3,77 +3,57 @@ package edu.ucla.library.prl.harvester.verticles;
 
 import static info.freelibrary.util.Constants.INADDR_ANY;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import edu.ucla.library.prl.harvester.Config;
 
 import info.freelibrary.util.HTTP;
 
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.RunTestOnContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.core.Vertx;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 
 /**
  * Tests the main verticle of the Vert.x application.
  */
-@RunWith(VertxUnitRunner.class)
+@ExtendWith(VertxExtension.class)
+@TestInstance(Lifecycle.PER_CLASS)
 public class MainVerticleTest {
-
-    /**
-     * Rule that creates the test context.
-     */
-    @Rule
-    public RunTestOnContext myContext = new RunTestOnContext();
 
     /**
      * Sets up the test.
      *
+     * @param aVertx A Vert.x instance
      * @param aContext A test context
      */
-    @Before
-    public void setUp(final TestContext aContext) {
-        final int port = Integer.parseInt(System.getenv(Config.HTTP_PORT));
-        final DeploymentOptions options = new DeploymentOptions();
-        final Async asyncTask = aContext.async();
-
-        aContext.put(Config.HTTP_PORT, port);
-        options.setConfig(new JsonObject().put(Config.HTTP_PORT, port));
-
-        myContext.vertx().deployVerticle(MainVerticle.class.getName(), options)
-                .onSuccess(result -> asyncTask.complete()).onFailure(aContext::fail);
+    @BeforeAll
+    public void setUp(final Vertx aVertx, final VertxTestContext aContext) {
+        aVertx.deployVerticle(MainVerticle.class.getName()).onSuccess(unused -> aContext.completeNow())
+                .onFailure(aContext::failNow);
     }
 
     /**
      * Tests the server can start successfully.
      *
+     * @param aVertx A Vert.x instance
      * @param aContext A test context
      */
     @Test
-    public void testThatTheServerIsStarted(final TestContext aContext) {
-        final WebClient client = WebClient.create(myContext.vertx());
-        final Async asyncTask = aContext.async();
+    public void testThatTheServerIsStarted(final Vertx aVertx, final VertxTestContext aContext) {
+        final WebClient client = WebClient.create(aVertx);
+        final int port = Integer.parseInt(System.getenv(Config.HTTP_PORT));
 
-        client.get(aContext.get(Config.HTTP_PORT), INADDR_ANY, "/status").send().onSuccess(response -> {
-            aContext.assertEquals(HTTP.OK, response.statusCode());
-            complete(asyncTask);
-        }).onFailure(aContext::fail);
-    }
-
-    /**
-     * A convenience method to end asynchronous tasks.
-     *
-     * @param aAsyncTask A task to complete
-     */
-    private void complete(final Async aAsyncTask) {
-        if (!aAsyncTask.isCompleted()) {
-            aAsyncTask.complete();
-        }
+        client.get(port, INADDR_ANY, "/status").send().onSuccess(response -> {
+            aContext.verify(() -> {
+                assertEquals(HTTP.OK, response.statusCode());
+            }).completeNow();
+        }).onFailure(aContext::failNow);
     }
 }
