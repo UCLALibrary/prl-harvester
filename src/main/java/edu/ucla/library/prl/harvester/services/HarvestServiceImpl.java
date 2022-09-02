a54d3367fd0899514788fc87b83339d7661cf50c
David Rickard <drickard1967@library.ucla.edu>
2022-09-02 17:36:40 +0000

    stub implementations

---
+++ src/main/java/edu/ucla/library/prl/harvester/services/HarvestServiceImpl.java
@@ -0,0 +1,31 @@
+
+package edu.ucla.library.prl.harvester.services;
+
+import edu.ucla.library.prl.harvester.Job;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * The interface of the event bus service that runs harvest jobs.
+ */
+public class HarvestServiceImpl implements HarvestService {
+
+    @SuppressWarnings("PMD.UnusedFormalParameter") //temp until constructor defined
+    HarvestServiceImpl(final Vertx aVertx, final JsonObject aConfig) {
+        // TODO: code class constructor
+    }
+
+    @Override
+    public Future<Object> run(final Job aJob) {
+        // TODO implement method
+        return Future.succeededFuture(null);
+    }
+
+    @Override
+    public Future<Void> close() {
+        // TODO implement method
+        return Future.succeededFuture(null);
+    }
+}
