e840f2d3a295a40bc431e1b86cc1a64457caa03c
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2022-10-10 12:15:29 -0700

    Make curl commands more readable
    Also remove unnecessary form fields, and don't use -F (for consistency).

--- src/test/docker/provider-initializer/docker-entrypoint.sh
+++ src/test/docker/provider-initializer/docker-entrypoint.sh
@@ -14,10 +14,8 @@ configure_repository_info() {
     echo -e "`bold '--- Repository info configuration ---'`\n"
 
     curl -s -X POST ${JOAI_SERVICE_URL}/admin/repository_info-validate.do \
-        -F 'repositoryName=Test Institution' \
-        -F 'adminEmail=test@example.edu' \
-        -F 'repositoryDescription=' \
-        -F 'namespaceIdentifier=' \
+        --data-urlencode 'repositoryName=Test Institution' \
+        --data-urlencode 'adminEmail=test@example.edu' \
     | render_joai_response
 
     echo
@@ -28,14 +26,16 @@ add_metadata_dir() {
 
     echo -e "`bold '--- Metadata validation ---'`\n"
 
-    # This jOAI endpoint seems to require the data as application/x-www-form-urlencoded, so we can't pass individual input fields via -F
     # The call to sed is necessary because html-tidy complains about an unmatched </form>, and <br> would complicate rendering the table as plaintext
     curl -s -X POST ${JOAI_SERVICE_URL}/admin/metadata_dir-validate.do \
-        --data-raw "command=addMetadataDir&dirNickname=Test+Set+%22${set_spec}%22&dirMetadataFormat=oai_dc&dirPath=%2Fjoai%2Fdata%2F${set_spec}&metadataNamespace=http%3A%2F%2Fwww.openarchives.org%2FOAI%2F2.0%2Foai_dc%2F&metadataSchema=http%3A%2F%2Fwww.openarchives.org%2FOAI%2F2.0%2Foai_dc.xsd" \
+        --data-raw "command=addMetadataDir&dirMetadataFormat=oai_dc" \
+        --data-urlencode "dirNickname=Test Set \"${set_spec}\"" \
+        --data-urlencode "dirPath=/joai/data/${set_spec}" \
     | sed -e 's/<\/form>//' -e 's/<br>/ /' \
     | render_joai_response
 
     echo
+#--data-urlencode "metadataNamespace=http://www.openarchives.org/OAI/2.0/oai_dc/&metadataSchema=http://www.openarchives.org/OAI/2.0/oai_dc.xsd" \
 }
 
 define_set() {
@@ -44,10 +44,12 @@ define_set() {
 
     echo -e "`bold '--- Set definition ---'`\n"
 
-    # This jOAI endpoint seems to require the data as application/x-www-form-urlencoded, so we can't pass individual input fields via -F
     # The call to sed is necessary because html-tidy complains about an unmatched </form>, and <br> would complicate rendering the table as plaintext
     curl -s -X POST ${JOAI_SERVICE_URL}/admin/set_definition-validate.do \
-        --data-raw "setName=Test+Set+%22${set_spec}%22&setSpec=${set_spec}&include_radio=include_radio_3&includedDirs=%2Fjoai%2Fdata%2F${set_spec}&limit_radio=limit_radio_1&exclude_radio=exclude_radio_1" \
+        --data-urlencode "setName=Test Set \"${set_spec}\"" \
+        --data-urlencode "includedDirs=/joai/data/${set_spec}" \
+        --data-raw "setSpec=${set_spec}" \
+        --data-raw "include_radio=include_radio_3&limit_radio=limit_radio_1&exclude_radio=exclude_radio_1" \
     | sed -e 's/<\/form>//' -e 's/<br>/ /' \
     | render_joai_response
 
