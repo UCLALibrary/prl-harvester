61c79529764820358834100d5a3d74b55272a1c6
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2022-11-01 17:25:37 -0700

    Add solr-initializer container to configure Solr

---
+++ src/main/resources/scripts/start-solr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script closely follows the "Custom Set-Up Scripts" example here:
+# https://solr.apache.org/guide/solr/9_0/deployment-guide/solr-in-docker.html#custom-set-up-scripts
+
+precreate-core prl && \
+
+# See: https://solr.apache.org/guide/solr/9_0/indexing-guide/tokenizers.html#icu-tokenizer
+mkdir /var/solr/data/lib && \
+ln -s -t /var/solr/data/lib \
+    /opt/solr/modules/analysis-extras/lib/{solr-analysis-extras-*.jar,icu4j-*.jar,lucene-analysis-icu-*.jar} && \
+
+solr-foreground
