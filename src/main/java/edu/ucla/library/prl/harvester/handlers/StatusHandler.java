0aa8c3487785dc80699633fe09a89f27045f8e3c
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2022-12-13 14:17:58 -0800

    Remove unused instance variables

--- src/main/java/edu/ucla/library/prl/harvester/handlers/StatusHandler.java
+++ src/main/java/edu/ucla/library/prl/harvester/handlers/StatusHandler.java
@@ -8,7 +8,6 @@ import edu.ucla.library.prl.harvester.JsonKeys;
 import info.freelibrary.util.HTTP;
 
 import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
@@ -18,32 +17,9 @@ import io.vertx.ext.web.RoutingContext;
  */
 public class StatusHandler implements Handler<RoutingContext> {
 
-    /**
-     * The handler's copy of the Vert.x instance.
-     */
-    private final Vertx myVertx;
-
-    /**
-     * Creates a handler that returns a status response.
-     *
-     * @param aVertx A Vert.x instance
-     */
-    public StatusHandler(final Vertx aVertx) {
-        myVertx = aVertx;
-    }
-
     @Override
     public void handle(final RoutingContext aContext) {
         aContext.response().setStatusCode(HTTP.OK).putHeader(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON.toString())
                 .end(new JsonObject().put(JsonKeys.STATUS, "ok").encodePrettily());
-    }
-
-    /**
-     * Gets the Vert.x instance associated with this handler.
-     *
-     * @return The Vert.x instance associated with this handler
-     */
-    public Vertx getVertx() {
-        return myVertx;
     }
 }
