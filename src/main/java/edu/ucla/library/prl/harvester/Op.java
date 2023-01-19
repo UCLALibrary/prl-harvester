80f47416b54fbb833308535b61b03dc3395943ea
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2023-01-18 16:04:03 -0800

    Use an enum for OpenAPI operations
    It's a useful property that the field names exactly match the OpenAPI
    operation ids, so warnings suppressed.

--- src/main/java/edu/ucla/library/prl/harvester/Op.java
+++ src/main/java/edu/ucla/library/prl/harvester/Op.java
@@ -4,17 +4,11 @@ package edu.ucla.library.prl.harvester;
 /**
  * OpenAPI operation IDs.
  */
-public final class Op {
+@SuppressWarnings("PMD.FieldNamingConventions")
+public enum Op {
 
     /**
      * A constant for the "get status" operation.
      */
-    public static final String GET_STATUS = "getStatus";
-
-    /**
-     * Constants class' constructors should be private.
-     */
-    private Op() {
-    }
-
+    getStatus;
 }
