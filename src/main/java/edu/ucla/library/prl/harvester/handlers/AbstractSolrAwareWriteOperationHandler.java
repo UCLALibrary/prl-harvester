32eb7b9544bdf8a94a353401335cba6266abc2f2
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2023-03-09 13:45:00 -0800

    Keep Solr in sync with Postgres

---
+++ src/main/java/edu/ucla/library/prl/harvester/handlers/AbstractSolrAwareWriteOperationHandler.java
@@ -0,0 +1,49 @@
+
+package edu.ucla.library.prl.harvester.handlers;
+
+import org.apache.solr.client.solrj.response.UpdateResponse;
+
+import edu.ucla.library.prl.harvester.Config;
+
+import io.ino.solrs.JavaAsyncSolrClient;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.sqlclient.Tuple;
+
+/**
+ * An abstract base class for write operation request handlers that need to update Solr.
+ */
+public abstract class AbstractSolrAwareWriteOperationHandler extends AbstractRequestHandler {
+
+    /**
+     * A client for sending institution records to Solr.
+     */
+    protected final JavaAsyncSolrClient mySolrClient;
+
+    /**
+     * The Vert.x instance.
+     */
+    protected final Vertx myVertx;
+
+    /**
+     * @param aVertx A Vert.x instance
+     * @param aConfig A configuration
+     */
+    protected AbstractSolrAwareWriteOperationHandler(final Vertx aVertx, final JsonObject aConfig) {
+        super(aVertx);
+
+        mySolrClient = JavaAsyncSolrClient.create(aConfig.getString(Config.SOLR_CORE_URL));
+        myVertx = aVertx;
+    }
+
+    /**
+     * Performs a Solr update appropriate for the type of incoming request.
+     *
+     * @param aData The relevant data for the Solr operation; implementers may use as needed and are responsible for
+     *        reading it carefully
+     * @return The result of performing the Solr update
+     */
+    abstract Future<UpdateResponse> updateSolr(Tuple aData);
+}
--- src/main/java/edu/ucla/library/prl/harvester/handlers/RemoveJobHandler.java
+++ src/main/java/edu/ucla/library/prl/harvester/handlers/RemoveJobHandler.java
@@ -1,24 +1,39 @@
 
 package edu.ucla.library.prl.harvester.handlers;
 
-import org.apache.http.HttpStatus;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
+import org.apache.http.HttpStatus;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+
+import edu.ucla.library.prl.harvester.Institution;
+import edu.ucla.library.prl.harvester.Job;
+import edu.ucla.library.prl.harvester.OaipmhUtils;
 import edu.ucla.library.prl.harvester.Param;
 
+import info.freelibrary.util.StringUtils;
+
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.sqlclient.Tuple;
 
 /**
  * A handler for removing jobs.
  */
-public final class RemoveJobHandler extends AbstractRequestHandler {
+public final class RemoveJobHandler extends AbstractSolrAwareWriteOperationHandler {
 
     /**
      * @param aVertx A Vert.x instance
+     * @param aConfig A configuration
      */
-    public RemoveJobHandler(final Vertx aVertx) {
-        super(aVertx);
+    public RemoveJobHandler(final Vertx aVertx, final JsonObject aConfig) {
+        super(aVertx, aConfig);
     }
 
     @Override
@@ -28,13 +43,69 @@ public final class RemoveJobHandler extends AbstractRequestHandler {
         try {
             final int id = Integer.parseInt(aContext.request().getParam(Param.id.name()));
 
-            myHarvestScheduleStoreService.removeJob(id).compose(nil -> {
-                return myHarvestJobSchedulerService.removeJob(id);
-            }).onSuccess(nil -> {
+            // Query database first to get job info for removing it
+            myHarvestScheduleStoreService.getJob(id).compose(job -> {
+                return myHarvestScheduleStoreService.getInstitution(job.getInstitutionID()).compose(institution -> {
+                    // Do all the solr updating in this context
+                    return myHarvestScheduleStoreService.removeJob(id).compose(nil -> {
+                        return myHarvestJobSchedulerService.removeJob(id);
+                    }).compose(nil -> {
+                        return updateSolr(Tuple.of(job.toJson(), institution.toJson()));
+                    });
+                });
+            }).onSuccess(solrResponse -> {
                 response.setStatusCode(HttpStatus.SC_NO_CONTENT).end();
             }).onFailure(aContext::fail);
         } catch (final NumberFormatException details) {
             response.setStatusCode(HttpStatus.SC_BAD_REQUEST).end(details.getMessage());
         }
+    }
+
+    /**
+     * Removes all item records that were harvested by the job.
+     *
+     * @param aData A Tuple of the ID of the job to remove, and its JSON representation
+     */
+    @Override
+    Future<UpdateResponse> updateSolr(final Tuple aData) {
+        final Job job = new Job(aData.getJsonObject(0));
+        final Institution institution = new Institution(aData.getJsonObject(1));
+        final Future<String> futureRecordRemovalQuery =
+                getRecordRemovalQuery(myVertx, institution.getName(), job.getRepositoryBaseURL(), job.getSets());
+
+        return futureRecordRemovalQuery.compose(solrQuery -> {
+            final CompletionStage<UpdateResponse> removal =
+                    mySolrClient.deleteByQuery(solrQuery).thenCompose(result -> mySolrClient.commit());
+
+            return Future.fromCompletionStage(removal);
+        });
+    }
+
+    /**
+     * @param aVertx A Vert.x instance
+     * @param anInstitutionName An institution name
+     * @param aBaseURL An OAI-PMH repository base URL
+     * @param aSets A list of sets
+     * @return A Solr query that can be used to remove records from the given sets associated with the given institution
+     */
+    static Future<String> getRecordRemovalQuery(final Vertx aVertx, final String anInstitutionName, final URL aBaseURL,
+            final Optional<List<String>> aSets) {
+        final Future<List<String>> getSetsToRemove;
+
+        if (aSets.isPresent() && !aSets.get().isEmpty()) {
+            getSetsToRemove = Future.succeededFuture(aSets.get());
+        } else {
+            // Missing or empty list means all sets in the repository
+            getSetsToRemove = OaipmhUtils.listSets(aVertx, aBaseURL).map(OaipmhUtils::getSetSpecs);
+        }
+
+        return getSetsToRemove.map(sets -> {
+            final String[] collectionQueryClauses = sets.stream() //
+                    .map(set -> StringUtils.format("set_spec:\"{}\"", set)) //
+                    .toArray(x -> new String[sets.size()]);
+
+            return StringUtils.format("institutionName:\"{}\" AND ({})", anInstitutionName,
+                    String.join(" OR ", collectionQueryClauses));
+        });
     }
 }
