e01d7a43646737c2b78b620f80f4191cb6e8a059
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2023-03-14 18:46:02 -0700

    Implement UI for addInstitution

--- src/main/frontend/src/components/HarvesterAdmin.vue
+++ src/main/frontend/src/components/HarvesterAdmin.vue
@@ -3,6 +3,8 @@ import { reactive, ref, computed, provide } from "vue"
 import InstitutionItem from "./InstitutionItem.vue"
 
 const state = reactive({ institutions: {}, jobs: {} })
+const displayInstitutionForm = ref(false)
+const institutionToAdd = ref({})
 const institutionToRemove = ref()
 const actionResultAlert = ref()
 const sortedInstitutions = computed(() => Object.values(state.institutions).sort((a, b) => (a.name < b.name ? -1 : 1)))
@@ -27,6 +29,55 @@ fetch("/jobs")
             state.jobs[job.institutionID].push(job)
         })
     })
+
+/**
+ * Sets the component state that renders a dialog with a form for the user to add an institution.
+ */
+function toggleDisplayInstitutionForm() {
+    displayInstitutionForm.value = !displayInstitutionForm.value
+}
+
+/**
+ * Sets the component state that is used to populate the institution form.
+ *
+ * @param {Object} anInstitution The institution to add
+ */
+function setInstitutionToAdd(anInstitution) {
+    institutionToAdd.value = anInstitution
+}
+
+/**
+ * Adds an institution
+ *
+ * @param {Object} anInstitution The institution to add
+ */
+async function addInstitution(anInstitution) {
+    const response = await fetch("/institutions", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(anInstitution),
+    })
+
+    if (response.status === 201) {
+        const responseBody = await response.json()
+
+        state.institutions[responseBody.id] = responseBody
+
+        actionResultAlert.value = {
+            color: "success",
+            message: "Institution added successfully",
+        }
+    } else {
+        actionResultAlert.value = {
+            color: "error",
+            message: `Institution add failed: HTTP ${response.status} (${response.statusText})`,
+        }
+    }
+
+    // Clear the form and hide it
+    setInstitutionToAdd({})
+    toggleDisplayInstitutionForm()
+}
 
 /**
  * Sets the component state that renders a dialog for the user to confirm institution removal.
@@ -67,12 +118,32 @@ provide("setInstitutionToRemove", setInstitutionToRemove)
 </script>
 
 <template>
+    <v-btn color="primary" variant="outlined" @click="toggleDisplayInstitutionForm">Add Institution</v-btn>
+
     <ol v-if="hasInstitutions">
         <li v-for="institution in sortedInstitutions" :key="institution.id">
             <InstitutionItem v-bind="institution" :jobs="state.jobs[institution.id] || []" />
         </li>
     </ol>
     <p v-else>No institutions yet!</p>
+
+    <v-dialog v-model="displayInstitutionForm" width="768">
+        <v-card>
+            <v-form>
+                <v-text-field label="Name" v-model="institutionToAdd.name" required></v-text-field>
+                <v-textarea label="Description" v-model="institutionToAdd.description" required></v-textarea>
+                <v-text-field label="Location" v-model="institutionToAdd.location" required></v-text-field>
+                <v-text-field label="Website" type="url" v-model="institutionToAdd.website" required></v-text-field>
+                <v-text-field label="Email" type="email" v-model="institutionToAdd.email"></v-text-field>
+                <v-text-field label="Phone" type="tel" v-model="institutionToAdd.phone"></v-text-field>
+                <v-text-field label="Web Contact" type="url" v-model="institutionToAdd.webContact"></v-text-field>
+            </v-form>
+            <v-card-actions class="d-flex justify-center align-baseline">
+                <v-btn color="primary" variant="outlined" width="auto" @click="addInstitution(institutionToAdd)">Add</v-btn>
+                <v-btn color="error" variant="outlined" width="auto" @click="toggleDisplayInstitutionForm">Cancel</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 
     <v-dialog v-if="institutionToRemove" v-model="institutionToRemove" width="auto">
         <v-card>
